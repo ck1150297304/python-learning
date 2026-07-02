89ab3b83163d0101829b81c0e02b03fc5749ca47
ck1150297304 <ck1150297304@163.com>
2026-07-02 21:23:04 +0800

    Complete Day 4 module and package basics

---
+++ day04.py
@@ -0,0 +1,11 @@
+from utils import calculator
+
+
+def main():
+    print(calculator.add(5, 6))
+    print(calculator.subtract(8, 3))
+    print(calculator.multiply(4, 8))
+    print(calculator.divide(4, 8))
+
+if __name__ == '__main__':
+    main()
